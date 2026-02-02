5938e17a25d193c5c10f605ebfe105b0a0d2c919
Ryan Moreira <ryanmoreirap@gmail.com>
2026-02-02 19:29:51 -0300

    ecercicio 01

---
+++ Algoritmos e Técnicas de Programação/Exercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor do lado do quadrado:");
+            double lado = double.Parse(Console.ReadLine());
+
+            double perimetro = lado * 4;
+            double area = lado * lado;
+            double diagonal = lado * Math.Sqrt(2);
+
+            Console.WriteLine($"Perímetro: {perimetro}");
+            Console.WriteLine($"Área: {area}");
+            Console.WriteLine($"Diagonal: {diagonal}");
+            
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
