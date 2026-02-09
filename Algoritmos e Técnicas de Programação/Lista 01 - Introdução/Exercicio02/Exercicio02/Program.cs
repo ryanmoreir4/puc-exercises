2fda09c51995d10eef24910b70b497cd2c286373
Ryan Moreira <ryanmoreirap@gmail.com>
2026-02-08 22:06:26 -0300

    exercicios iniciais

---
+++ Algoritmos e Técnicas de Programação/Lista 01 - Introdução/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a base do retângulo:");
+            double baseRetangulo = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a altura do retângulo:");
+            double alturaRetangulo = double.Parse(Console.ReadLine());
+
+            double perimetro = 2 * (baseRetangulo + alturaRetangulo);
+            double area = baseRetangulo * alturaRetangulo;
+            double diagonal = Math.Sqrt(Math.Pow(baseRetangulo, 2) + Math.Pow(alturaRetangulo, 2));
+
+            Console.WriteLine($"Perimetro: {perimetro}");
+            Console.WriteLine($"Área: {area}");
+            Console.WriteLine($"Diagonal: {diagonal}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Algoritmos e Técnicas de Programação/Lista 01 - Introdução/Exericio03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exericio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura (em ºF) a ser convertida em ºC: ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            double celsius = 5 * (fahrenheit - 32) / 9;
+
+            Console.WriteLine($"{fahrenheit}ºF convertido em ºC é: {celsius}");
+
+        }
+    }
+}
