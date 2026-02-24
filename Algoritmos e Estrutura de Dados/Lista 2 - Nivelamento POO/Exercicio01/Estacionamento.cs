9dae5b05996df2cbadd20278c3ad395266060b92
Ryan Moreira <ryanmoreirap@gmail.com>
2026-02-24 09:31:39 -0300

    add exercises

---
+++ Algoritmos e Estrutura de Dados/Lista 2 - Nivelamento POO/Exercicio01/Estacionamento.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+  class Estacionamento
+  {
+    private string nome;
+    private int numVagasLivres;
+    private string[] vagas;
+
+    public Estacionamento(string nome, int numTotalVagas)
+    {
+        this.nome = nome;
+        this.numVagasLivres = numTotalVagas;
+        this.vagas = new string[numTotalVagas];
+    }
+
+    public int Estacionar(string placa)
+    {
+        if (numVagasLivres <= 0) return -1;
+
+        for (int i = 0; i < vagas.Length; i++)
+        {
+            if (vagas[i] == null)
+            {
+                vagas[i] = placa;
+                numVagasLivres--;
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int BuscarNumVaga(string placa)
+    {
+        int numVaga = 0;
+
+        for(int i = 0; i < vagas.Length; i++)
+        {
+            if (vagas[i] == placa)
+            {
+                numVaga = i;
+
+                return numVaga;
+            }
+        }
+        return -1;
+    }
+
+    public void Retirar(string placa)
+    {
+        for(int i = 0; i < vagas.Length; i++)
+        {
+            if (vagas[i] == placa)
+            {
+                vagas[i] = null;
+            }
+        }
+    }
+
+    public int NumVagasLivres
+    {
+        get { return numVagasLivres; }
+    }
+
+    public void ExibirOcupacao()
+    {
+        for(int i = 0; i < vagas.Length; i++)
+        {
+            if (vagas[i] != null)
+            {
+                Console.Write($"Vaga {i+1} -- {vagas[i]}\n");
+            }
+            Console.Write($"Vaga {i+1} -- Vazia\n");
+        }
+    }
+}
---
+++ Algoritmos e Estrutura de Dados/Lista 2 - Nivelamento POO/Exercicio01/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Teste
+{
+    static void Main(string[] args)
+    {
+        Estacionamento est = new Estacionamento("Central Park", 30);
+
+        est.Estacionar("ABC1234");
+        est.Estacionar("DEF5678");
+        est.Estacionar("GHI9012");
+        est.Estacionar("JKL3456");
+        est.Estacionar("MNO7890");
+
+        est.ExibirOcupacao();
+
+        Console.WriteLine("\nBuscar placa DEF5678:");
+        int vaga = est.BuscarNumVaga("DEF5678");
+
+        if (vaga != -1)
+        {
+            Console.WriteLine("Encontrado na vaga " + vaga);
+        }
+        else
+        {
+            Console.WriteLine("Não encontrado");
+        }
+
+        Console.WriteLine("\nRetirando placa DEF5678...");
+        est.Retirar("DEF5678");
+
+        Console.WriteLine("\nOcupação após remoção: ");
+        est.ExibirOcupacao();
+
+        est.Estacionar("PQR1111");
+        est.Estacionar("STU2222");
+        est.Estacionar("VWX3333");
+
+        Console.WriteLine("\nOcupação após as 3 inserções: ");
+        est.ExibirOcupacao();
+
+        Console.WriteLine($"\nVagas livres: {est.NumVagasLivres}");
+    }
+}
